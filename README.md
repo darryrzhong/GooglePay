bb0ba0a534f2643be03478a9972333cadab8d68b
darryrzhong <darryrzhong.dev@gmail.com>
2025-11-24 14:43:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GooglePay
+GooglePay 是一个基于 Google Play Billing Library 封装的 Android 支付库，旨在简化 Google 支付的接入流程，提供统一的接口管理一次性购买和订阅服务。
---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    rulesMode.set(RulesMode.FAIL_ON_PROJECT_RULES)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GooglePay"
+//可视化依赖lib
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+//使用共享构建服务而不通过方法声明要求的任务Task.usesService将发出弃用警告
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
+include(":app")
+//include(":gpPay")
